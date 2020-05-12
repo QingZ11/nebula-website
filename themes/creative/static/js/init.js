07fe93b0e3e291e0d6d3c9dcdb612f57808562c9
Jerry Liang <8124783+nianiaJR@users.noreply.github.com>
2020-05-12 14:52:25 +0800

    chore: hide popup (#167)

--- themes/creative/static/js/init.js
+++ themes/creative/static/js/init.js
@@ -9,22 +9,23 @@
   }
 
   // popup after some time
-  setTimeout(function (){
-    $('#J_Star_Popup').popover({
-      content: 
-        '<img class="cross" src="/images/cross.png"></img>' +
-        '<div class="item">' +
-          '<p><span class="emoj">🌟</span>Like what we do?</p>' +
-          '<p><a href="https://0x7.me/popup2github" target="_blank" onclick="gtag(\'event\', \'Link Click\', { event_category: \'Engagement\', event_label:  \'Star via popup\'})">Star us on GitHub</a></p>' +
-        '</div>' +
-        '<div class="item">' +
-          '<p><span class="emoj">🙋‍♂️</span>Have a problem?</p>' +
-          '<p><a href="https://discuss.nebula-graph.io" target="_blank">Ask us on our forum</a></p>' +
-        '</div>' +
-        '<img src="/images/popup-logo.png" class="popup-logo">',
-      html: true,
-      container: '#J_Star_Popup',
-      placement: 'top',
-    }).popover('show');
-  }, 1000 * 30);
+  // TODO: remove in future
+  // setTimeout(function (){
+  //   $('#J_Star_Popup').popover({
+  //     content: 
+  //       '<img class="cross" src="/images/cross.png"></img>' +
+  //       '<div class="item">' +
+  //         '<p><span class="emoj">🌟</span>Like what we do?</p>' +
+  //         '<p><a href="https://0x7.me/popup2github" target="_blank" onclick="gtag(\'event\', \'Link Click\', { event_category: \'Engagement\', event_label:  \'Star via popup\'})">Star us on GitHub</a></p>' +
+  //       '</div>' +
+  //       '<div class="item">' +
+  //         '<p><span class="emoj">🙋‍♂️</span>Have a problem?</p>' +
+  //         '<p><a href="https://discuss.nebula-graph.io" target="_blank">Ask us on our forum</a></p>' +
+  //       '</div>' +
+  //       '<img src="/images/popup-logo.png" class="popup-logo">',
+  //     html: true,
+  //     container: '#J_Star_Popup',
+  //     placement: 'top',
+  //   }).popover('show');
+  // }, 1000 * 30);
 })()
