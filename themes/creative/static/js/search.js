83472ef62aa0c599f96d714cfccdf449ddc70c2c
study <36290546+Mark-xing@users.noreply.github.com>
2020-08-19 11:46:35 +0800

    fix baidu search (#280)
    * fix baidu search
    
    * divide search en site and cn site

--- themes/creative/static/js/search.js
+++ themes/creative/static/js/search.js
@@ -6,15 +6,17 @@
         },
         attachEvent(){
             $('#search-btn').click(function(){
-                $('.baidu-iframe').css("display","block"); 
+                $('#bdcs-frame-box').css("display","block"); 
             });
             $('#close-icon').click(function(){
-                $('.baidu-iframe').css("display","none");
+                $('#bdcs-frame-box').css("display","none");
             })
         },
         renderGoogleSearch(){
             var googleHtml="<div class='gcse-search'></div><script src='https://cse.google.com/cse.js?cx=008473639027829005971:tevo895mqp8'>";
+            var googleCNHtml="<div class='gcse-search'></div><script src='https://cse.google.com/cse.js?cx=b97c3704ba815b2bb'>";
             $('#google-search').append(googleHtml);
+            $('#google-searchCN').append(googleCNHtml);
             $('#baidu-search').css('display','none');
         },
         renderBaiduSearch(){
